dac21382665dabd13ec5ba309cdf54982dcfd84e
Savannaha <savwiley@yahoo.com>
2021-06-27 12:55:15 -0500

    stylized header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from "react";
+import Header from "./components/Header.js";
 
 function App() {
+
   return (
     <>
-    hello
+      <Header />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Header = () => {
 
+  //header gradient moves with mouse
+  useEffect(() => {
+    const header = document.querySelector(".header");
+    header.addEventListener("mousemove", (e) => {
+      const X = e.clientX / window.innerWidth * 100;
+      const Y = e.clientY / window.innerHeight * 100;
+      header.style.background = `radial-gradient(at ${X}% ${Y}%, #0ec7a8, #036656)`;
+    });
+  })
+
+  //profession title changes
+  useEffect(() => {
+    const profession = document.querySelector("#webD");
+    profession.textContent = "Web Developer";
+    const change = () => {
+      let content = profession.textContent;
+      if (content === "Web Developer") {
+        profession.textContent = "Writer";
+      } else if (content === "Writer") {
+        profession.textContent = "Editor";
+      } else if (content === "Editor") {
+        profession.textContent = "UI/UX Designer";
+      } else if (content === "UI/UX Designer") {
+        profession.textContent = "Web Developer";
+      }
+    }
+    setInterval(change, 1000);
+  })
+
   return (
-    <div className="header"></div>
+    <div className="header">
+      <div className="headerInner">
+        <div id="name">Savannaha Wiley</div>
+        <div id="webD"></div>
+      </div>
+    </div>
   )
 }
 
