47ed64eba16020ae3ae76544f18c1ddf8b9b7d9b
Savannaha <savwiley@yahoo.com>
2021-06-26 20:04:17 -0500

    created Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import React, { useEffect } from "react";
+import Header from "./components/Header.js";
 
 function App() {
+
+
+  useEffect(() => {
+    const header = document.querySelector(".header");
+
+    for (let i = 0; i < 5; i++) {
+      const bubble = document.createElement("div");
+      bubble.setAttribute("class", "bubble");
+      bubble.setAttribute("id", `bubble${i}`)
+      header.appendChild(bubble);
+    }
+  })
+
   return (
     <>
-    hello
+      <Header />
     </>
   );
 }
