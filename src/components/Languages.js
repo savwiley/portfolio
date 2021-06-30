0e51fd8b5f67cc07db886c806c87167cd71ecc95
Savannaha <savwiley@yahoo.com>
2021-06-30 16:51:04 -0500

    rearranged sections

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -24,79 +24,83 @@ const Languages = () => {
   return(
     <div className="languages interactive">
 
-      <div className="sect">Languages</div>
-      <div className="sectDiv">
-        <div id="html" data-tip="HTML5" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(htmlIcon.current)}} 
-        />
-        <div id="css" data-tip="CSS3" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(cssIcon.current)}} 
-        />
-        <div id="js" data-tip="JavaScript" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
-        />
-        <div id="php" data-tip="PHP" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(phpIcon.current)}} 
-        />
+      <div>
+        <div className="sect">Languages</div>
+        <div className="sectDiv">
+          <div id="html" data-tip="HTML5" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(htmlIcon.current)}} 
+          />
+          <div id="css" data-tip="CSS3" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(cssIcon.current)}} 
+          />
+          <div id="js" data-tip="JavaScript" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+          />
+          <div id="php" data-tip="PHP" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(phpIcon.current)}} 
+          />
+        </div>
+
+        <div className="sect">Front-End</div>
+        <div className="sectDiv">
+          <div id="webpack" data-tip="webpack" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(webpackIcon.current)}} 
+          />
+          <div id="react" data-tip="React" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactIcon.current)}} 
+          />
+          <div id="reactroute" data-tip="React-Router" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactRIcon.current)}} 
+          />
+        </div>
+
+        <div className="sect">Back-End</div>
+        <div className="sectDiv">
+          <div id="firebase" data-tip="Firebase" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(firebaseIcon.current)}} 
+          />
+          <div id="heroku" data-tip="Heroku" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(herokuIcon.current)}} 
+          />
+        </div>
       </div>
 
-      <div className="sect">Front-End</div>
-      <div className="sectDiv">
-        <div id="webpack" data-tip="webpack" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(webpackIcon.current)}} 
-        />
-        <div id="react" data-tip="React" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactIcon.current)}} 
-        />
-        <div id="reactroute" data-tip="React-Router" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactRIcon.current)}} 
-        />
-      </div>
+      <div>
+        <div className="sect">Testing</div>
+        <div className="sectDiv">
+          <div id="jasmine" data-tip="Jasmine" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jasmineIcon.current)}} 
+          />
+          <div id="jest" data-tip="Jest" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jestIcon.current)}} 
+          />
+        </div>
 
-      <div className="sect">Back-End</div>
-      <div className="sectDiv">
-        <div id="firebase" data-tip="Firebase" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(firebaseIcon.current)}} 
-        />
-        <div id="heroku" data-tip="Heroku" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(herokuIcon.current)}} 
-        />
-      </div>
+        <div className="sect">Formating</div>
+        <div className="sectDiv">
+          <div id="prettier" data-tip="Prettier" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(prettierIcon.current)}} 
+          />
+          <div id="eslint" data-tip="ESLint" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(eslintIcon.current)}} 
+          />
+        </div>
 
-      <div className="sect">Testing</div>
-      <div className="sectDiv">
-        <div id="jasmine" data-tip="Jasmine" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jasmineIcon.current)}} 
-        />
-        <div id="jest" data-tip="Jest" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jestIcon.current)}} 
-        />
-      </div>
-
-      <div className="sect">Formating</div>
-      <div className="sectDiv">
-        <div id="prettier" data-tip="Prettier" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(prettierIcon.current)}} 
-        />
-        <div id="eslint" data-tip="ESLint" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(eslintIcon.current)}} 
-        />
-      </div>
-
-      <div className="sect">Misc.</div>
-      <div className="sectDiv">
-        <div id="vscode" data-tip="VSCode" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(vscodeIcon.current)}} 
-        />
-        <div id="npm" data-tip="npm" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(npmIcon.current)}} 
-        />
-        <div id="yarn" data-tip="yarn" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(yarnIcon.current)}} 
-        />
-        <div id="git" data-tip="Git" 
-          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(gitIcon.current)}} 
-        />
+        <div className="sect">Misc.</div>
+        <div className="sectDiv">
+          <div id="vscode" data-tip="VSCode" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(vscodeIcon.current)}} 
+          />
+          <div id="npm" data-tip="npm" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(npmIcon.current)}} 
+          />
+          <div id="yarn" data-tip="yarn" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(yarnIcon.current)}} 
+          />
+          <div id="git" data-tip="Git" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(gitIcon.current)}} 
+          />
+        </div>
       </div>
     </div>
   )
