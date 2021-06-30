fdebc684bc151bf38c666086c3d0b8f81a93aa43
Savannaha <savwiley@yahoo.com>
2021-06-30 12:58:21 -0500

    refactored for DOMPurify & added more projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,54 +1,14 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import simpleIcons from "simple-icons";
+import DOMPurify from "dompurify";
 
 const Projects = () => {
-  const cssIcon = useRef(simpleIcons.Get('css3').svg);
   const jsIcon = useRef(simpleIcons.Get('javascript').svg);
+  const webpackIcon = useRef(simpleIcons.Get('webpack').svg);
   const reactIcon = useRef(simpleIcons.Get('react').svg);
   const reactRIcon = useRef(simpleIcons.Get('reactrouter').svg);
   const firebaseIcon = useRef(simpleIcons.Get('firebase').svg);
-  const vscodeIcon = useRef(simpleIcons.Get('visualstudiocode').svg);
-  const npmIcon = useRef(simpleIcons.Get('npm').svg);
-  const gitIcon = useRef(simpleIcons.Get('git').svg);
-  
-  
-  useEffect(() => {
-    //DOMS
-    //language
-    //const html = document.querySelector(".html");
-    const css = document.querySelector(".css");
-    const js = document.querySelector(".js");
-    //frontend
-    //const webpack = document.querySelector(".webpack");
-    const react = document.querySelector(".react");
-    const reactRoute = document.querySelector(".reactroute");
-    //backend
-    const firebase = document.querySelector(".firebase");
-    //misc
-    const vscode = document.querySelector(".vscode");
-    const npm = document.querySelector(".npm");
-    //const yarn = document.querySelector(".yarn");
-    const git = document.querySelector(".git");
-    //const jest = document.querySelector(".jest");
-
-    //PLACEMENT
-    //languages
-    //html.innerHTML = htmlIcon.svg;
-    css.innerHTML = cssIcon.current;
-    js.innerHTML = jsIcon.current;
-    //frontend
-    //webpack.innerHTML = webpackIcon.svg;
-    react.innerHTML = reactIcon.current;
-    reactRoute.innerHTML = reactRIcon.current;
-    //backend
-    firebase.innerHTML = firebaseIcon.current;
-    //misc
-    vscode.innerHTML = vscodeIcon.current;
-    npm.innerHTML = npmIcon.current;
-    //yarn.innerHTML = yarnIcon.svg;
-    git.innerHTML = gitIcon.current;
-    //jest.innerHTML = jestIcon.svg;
-  }, [])
+  const jestIcon = useRef(simpleIcons.Get('jest').svg);
 
   return(
     <div className="projects">
@@ -59,14 +19,18 @@ const Projects = () => {
           <div>Where's Waldo?</div>
           <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
           <div>
-            <span className="css"></span>
-            <span className="js"></span>
-            <span className="react"></span>
-            <span className="reactroute"></span>
-            <span className="firebase"></span>
-            <span className="vscode"></span>
-            <span className="npm"></span>
-            <span className="git"></span>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="react" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactIcon.current)}} 
+            />
+            <span className="reactroute" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactRIcon.current)}} 
+            />
+            <span className="firebase" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(firebaseIcon.current)}} 
+            />
           </div>
           <div>
             <a href="https://github.com/savwiley/waldo">GitHub</a>
@@ -77,24 +41,134 @@ const Projects = () => {
 
       <div className="proj">
       <div className="desc">
-          <div>Where's Waldo?</div>
+          <div>Battleship</div>
           <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
           <div>
-            <span className="css"></span>
-            <span className="js"></span>
-            <span className="react"></span>
-            <span className="reactroute"></span>
-            <span className="firebase"></span>
-            <span className="vscode"></span>
-            <span className="npm"></span>
-            <span className="git"></span>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="react" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactIcon.current)}} 
+            />
+            <span className="reactroute" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactRIcon.current)}} 
+            />
+            <span className="jest" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jestIcon.current)}} 
+            />
           </div>
           <div>
-            <a href="https://github.com/savwiley/waldo">GitHub</a>
-            <a href="https://waldo-9e96c.web.app/">Live Preview</a>
+            <a href="https://github.com/savwiley/battleship">GitHub</a>
+            <a href="https://savwiley.github.io/battleship/">Live Preview</a>
           </div>
         </div>
         <div className="pic">hey</div>
+      </div>
+
+      <div className="proj">
+        <div className="pic">hi</div>
+        <div className="desc">
+          <div>Mock Retail Site</div>
+          <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
+          <div>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="react" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactIcon.current)}} 
+            />
+            <span className="reactroute" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactRIcon.current)}} 
+            />
+          </div>
+          <div>
+            <a href="https://github.com/savwiley/shopping-cart">GitHub</a>
+            <a href="https://savwiley.github.io/shopping-cart/">Live Preview</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="proj">
+      <div className="desc">
+          <div>Memory Game</div>
+          <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
+          <div>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="react" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(reactIcon.current)}} 
+            />
+          </div>
+          <div>
+            <a href="https://github.com/savwiley/memoryCard">GitHub</a>
+            <a href="https://savwiley.github.io/memoryCard/">Live Preview</a>
+          </div>
+        </div>
+        <div className="pic">hey</div>
+      </div>
+
+      <div className="proj">
+        <div className="pic">hi</div>
+        <div className="desc">
+          <div>The Library</div>
+          <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
+          <div>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="webpack" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(webpackIcon.current)}} 
+            />
+            <span className="firebase" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(firebaseIcon.current)}} 
+            />
+          </div>
+          <div>
+            <a href="https://github.com/savwiley/libraryProject">GitHub</a>
+            <a href="https://savwiley.github.io/libraryProject/dist/">Live Preview</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="proj">
+      <div className="desc">
+          <div>To-Do List</div>
+          <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
+          <div>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="webpack" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(webpackIcon.current)}} 
+            />
+          </div>
+          <div>
+            <a href="https://github.com/savwiley/todos">GitHub</a>
+            <a href="https://savwiley.github.io/todos/dist/">Live Preview</a>
+          </div>
+        </div>
+        <div className="pic">hey</div>
+      </div>
+
+      <div className="proj">
+        <div className="pic">hi</div>
+        <div className="desc">
+          <div>Weather App</div>
+          <div>Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project. Where's Waldo-espue game with high scores table & three difficulties. TOP project.</div>
+          <div>
+            <span className="js" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(jsIcon.current)}} 
+            />
+            <span className="webpack" 
+            dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(webpackIcon.current)}} 
+            />
+          </div>
+          <div>
+            <a href="https://github.com/savwiley/weather">GitHub</a>
+            <a href="https://savwiley.github.io/weather/dist/">Live Preview</a>
+          </div>
+        </div>
       </div>
 
     </div>
