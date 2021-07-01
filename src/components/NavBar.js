f5068fb0d28f8647159a446a1a817df69f6ca3a8
Savannaha <savwiley@yahoo.com>
2021-07-01 18:48:24 -0500

    created navigation bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+
+  return(
+    <div className="navigation">
+      <a href="#top" alt="Go to Top of Page">back to top</a>
+      <a href="#languages" alt="Languages, Programs, & Projects">languages/codes</a>
+      <a href="#about" alt="About Me & Social Media">about/contact</a>
+    </div>
+  )
+}
+
+export default NavBar;
