1172dd7623e9af5efa516f1222bc4f646e9f36cb
Savannaha <savwiley@yahoo.com>
2021-07-01 15:42:49 -0500

    tried out different interactive backgrounds

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,18 @@ const Header = () => {
   //profession title changes
   useEffect(() => {
     const profession = document.querySelector("#webD");
-    profession.textContent = "Web Developer";
+    profession.textContent = "web developer";
     //consider changing to case/switch
     const change = () => {
       let content = profession.textContent;
-      if (content === "Web Developer") {
-        profession.textContent = "Writer";
-      } else if (content === "Writer") {
-        profession.textContent = "Editor";
-      } else if (content === "Editor") {
-        profession.textContent = "UI/UX Designer";
-      } else if (content === "UI/UX Designer") {
-        profession.textContent = "Web Developer";
+      if (content === "web developer") {
+        profession.textContent = "writer";
+      } else if (content === "writer") {
+        profession.textContent = "editor";
+      } else if (content === "editor") {
+        profession.textContent = "ui/ux designer";
+      } else if (content === "ui/ux designer") {
+        profession.textContent = "web developer";
       }
     };
     setInterval(change, 1000);
