8418b11d10f542796248e67fb5b550f32dbfbf78
Savannaha <savwiley@yahoo.com>
2021-07-01 13:10:33 -0500

    ran prettier

--- src/components/Extras.js
+++ src/components/Extras.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGraduationCap, faCode, faBirthdayCake, faRibbon, faKeyboard, faPenFancy } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGraduationCap,
+  faCode,
+  faBirthdayCake,
+  faRibbon,
+  faKeyboard,
+  faPenFancy,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Extras = () => {
-
-  return(
+  return (
     <div className="experience interactive">
-
       <div className="half">
         <div>
           <div className="faIcon">
             <FontAwesomeIcon icon={faGraduationCap} className="exIcon" />
           </div>
-          <div className="exBlurb">Where's Waldo-espue game with high scores table & three
-            difficulties. TOP project. Where's Waldo-espue game with high scores
-            table & three difficulties. TOP project. Where's Waldo-espue game
-            with high scores table & three difficulties. TOP project. Where's
-            Waldo-espue game with high scores table & three difficulties. TOP
-            project. Where's Waldo-espue game with high scores table & three
-            difficulties. TOP project. Where's Waldo-espue game with high scores
-            table & three difficulties. TOP project.</div>
+          <div className="exBlurb">Education</div>
         </div>
         <div>
           <div className="faIcon">
             <FontAwesomeIcon icon={faBirthdayCake} className="exIcon" />
           </div>
-          <div className="exBlurb"></div>
+          <div className="exBlurb">KillTheCake</div>
         </div>
         <div>
           <div className="faIcon">
             <FontAwesomeIcon icon={faKeyboard} className="exIcon" />
           </div>
-          <div className="exBlurb"></div>
+          <div className="exBlurb">Typing?</div>
         </div>
       </div>
 
@@ -40,39 +38,27 @@ const Extras = () => {
           <div className="faIcon">
             <FontAwesomeIcon icon={faCode} className="exIcon" />
           </div>
-          <div className="exBlurb">Where's Waldo-espue game with high scores table & three
-            difficulties. TOP project. Where's Waldo-espue game with high scores
-            table & three difficulties. TOP project. Where's Waldo-espue game
-            with high scores table & three difficulties. TOP project. Where's
-            Waldo-espue game with high scores table & three difficulties. TOP
-            project. Where's Waldo-espue game with high scores table & three
-            difficulties. TOP project. Where's Waldo-espue game with high scores
-            table & three difficulties. TOP project.</div>
+          <div className="exBlurb">TOP</div>
         </div>
         <div>
           <div className="faIcon">
             <FontAwesomeIcon icon={faRibbon} className="exIcon" />
           </div>
-          <div className="exBlurb"></div>
+          <div className="exBlurb">Charity</div>
         </div>
         <div>
           <div className="faIcon">
             <FontAwesomeIcon icon={faPenFancy} className="exIcon" />
           </div>
-          <div className="exBlurb"></div>
+          <div className="exBlurb">Writing?</div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Extras;
 
-
 /**
- * education
- * top
- * killthecake
- * charity
- * typing speed?
+ * should i put more info in a data-tip?
  */
