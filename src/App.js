acbe534e3ef55747421a55ac10197bd99de2c71e
Savannaha <savwiley@yahoo.com>
2021-07-04 17:46:32 -0500

    changed wording, ran prettier

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  console.log("This site was made with ReactJS & is hosted with Firebase. You can find all of the files at:");
+  console.log(
+    "This site was made with ReactJS & is hosted with Firebase. You can find all of the files at:"
+  );
   console.log("https://github.com/savwiley/portfolio");
   console.log("(っ ᵔ︠ ▿ ︡ᵔ)っ🎔 Thanks for visiting!");
 
@@ -41,9 +43,11 @@ function App() {
       <div className="blurb">
         <div id="piece">
           Hello there! My name, as you can likely guess, is Savannaha Wiley. I live in Louisiana with my two dogs and cat: Hank, Sadie, & Mini respectively. I finished college with a <b>Master's degree in 2017</b> & have worked in Technical Writing ever since.
-          <br /><br />
-          As my writings started leaning more into <b>software & coding</b>, I got an itch to learn more, prompting me to start with CSS. Once the pandemic happened and work slowed to a grinding halt, I jumped into the coding life practically face first & haven't regretted a second of it. Right now, other than learning as many coding languages as I can, my goal is to make the career change into <b>Web Development</b>.
-          <br /><br />
+          <br />
+          <br />
+          As my writings became more focused <b>software & coding</b>, I got an itch to learn more, prompting me to start with CSS. Once the pandemic happened and work slowed to a grinding halt, I jumped into the coding life practically face first & haven't regretted a second of it. Right now, other than learning as many coding languages as I can, my goal is to make the career change into <b>Web Development</b>.
+          <br />
+          <br />
           When I'm not coding, I enjoy playing FPS games with my mom, slogging through the entire Star Trek franchise with my best friend, playing TCGs with friends online, and informing my dog that he's a good boy. If you're interested in getting to know me better, you can check out my GitHub at the link below or look for me on <a href="#about">social media</a>. Regardless, I'm glad you stopped by!
         </div>
 
