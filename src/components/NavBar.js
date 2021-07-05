227df5897b531224dfdc3b3acf92d98ca56f15b9
Savannaha <savwiley@yahoo.com>
2021-07-05 16:50:04 -0500

    rearranged links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleUp } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
-    <div className="navigation">
-      <a href="#top" alt="Go to Top of Page">
-        Back to Top
-      </a>
-      <a href="#languages" alt="Languages, Programs, & Projects">
-        Languages/Codes
-      </a>
-      <a href="#about" alt="About Me & Social Media">
-        About/Contact
-      </a>
-    </div>
+    <>
+      <div className="navigation">
+        <a href="#languages" alt="Languages & Programs">
+          Languages
+        </a>
+        <a href="#projects" alt="Projects & Codes">
+          Projects
+        </a>
+        <a href="#socials" alt="Social Media & Contact">
+          Contact
+        </a>
+      </div>
+
+      <div id="up">
+        <a href="#top" alt="Go to Top of Page">
+          <FontAwesomeIcon icon={faArrowAltCircleUp} />
+        </a>
+      </div>
+    </>
   );
 };
 
